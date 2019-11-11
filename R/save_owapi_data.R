ecbb3c65e37bd82bab55405552c9af86ad6c1859
ben_bellman <benjamin_bellman@brown.edu>
2019-11-11 12:10:03 -0500

    Fixed bug that overwrote files with new query

--- R/save_owapi_data.R
+++ R/save_owapi_data.R
@@ -1,12 +1,10 @@
 ### NOTES
 
-# need to re-factor this to accept a location argument that
-# lets the user decide where to save the data
-# eventually, I want it to support databases
+# eventually, I want it to support database connections for saving
+# this means automatic DB table setup, if possible
 
 # need a function to do a specific test for valid file path / data (or if its a database connection)
 
-# need a function to confirm everything went ok before overwriting data
 
 
 # main function for saving data from queries
@@ -14,7 +12,7 @@ save_owapi_data <- function(output, file_path){
   suppressMessages(
 
     # if no files where data gets saved, create new ones
-    if (length(file_path) == 0){
+    if (length(list.files(file_path)) == 0){
       for (a in c("profile", hero_names())){
         readr::write_csv(output[[a]], paste0(file_path, "/", a, ".csv"))
       }
@@ -23,7 +21,7 @@ save_owapi_data <- function(output, file_path){
 
       for (a in c("profile", hero_names())){
         # check that table for hero exists, since new hero could have been released
-        if(a %in% list.files(paste0(file_path, "/", a, ".csv"))){
+        if(a %in% gsub(".csv", "", list.files(paste0(file_path)))){
           table <- suppressMessages(readr::read_csv(paste0(file_path, "/", a, ".csv")))
           table <- dplyr::bind_rows(table, output[[a]])
           readr::write_csv(table, paste0(file_path, "/", a, ".csv"))
