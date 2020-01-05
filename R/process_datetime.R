814246a8b1cba77cc9bbfad67538f4ae46b7b681
ben_bellman <benjamin_bellman@brown.edu>
2020-01-04 21:27:05 -0500

    Update process_datetime.R

--- R/process_datetime.R
+++ R/process_datetime.R
@@ -5,7 +5,7 @@ process_datetime <- function(datetime){
 
   # Setting the season using system time at time of query
   # Based on information from Retail Patch Notes on Dec 10, 2019
-  season <- case_when(
+  season <- dplyr::case_when(
     datetime < strptime("2020-01-02 13:00:00", format = "%Y-%m-%d %H:%M:%S", tz = "EST") ~ 19,
     datetime >= strptime("2020-01-02 13:00:00", format = "%Y-%m-%d %H:%M:%S", tz = "EST") ~ 20
   )
