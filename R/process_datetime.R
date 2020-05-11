87026570243cffb186d50c7e07f06e573260cff4
ben_bellman <benjamin_bellman@brown.edu>
2020-05-10 21:45:53 -0400

    Update for season 22

--- R/process_datetime.R
+++ R/process_datetime.R
@@ -9,7 +9,9 @@ process_datetime <- function(datetime){
     datetime < strptime("2020-01-02 13:00:00", format = "%Y-%m-%d %H:%M:%S", tz = "EST") ~ 19,
     datetime >= strptime("2020-01-02 13:00:00", format = "%Y-%m-%d %H:%M:%S", tz = "EST") &
       datetime < strptime("2020-03-05 13:00:00", format = "%Y-%m-%d %H:%M:%S", tz = "EST") ~ 20,
-    datetime >= strptime("2020-03-05 13:00:00", format = "%Y-%m-%d %H:%M:%S", tz = "EST") ~ 21
+    datetime >= strptime("2020-03-05 13:00:00", format = "%Y-%m-%d %H:%M:%S", tz = "EST") &
+      datetime < strptime("2020-05-07 13:00:00", format = "%Y-%m-%d %H:%M:%S", tz = "EST") ~ 21,
+    datetime >= strptime("2020-05-07 13:00:00", format = "%Y-%m-%d %H:%M:%S", tz = "EST") ~ 22
   )
 
   tibble::tibble(datetime, day, month, year, season)
